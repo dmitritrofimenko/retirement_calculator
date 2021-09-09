d2b8fc4dffcfe5b2614f4dfc0ebfedab989bb0a4
dmitri <dmitri.trofimenko@gmail.com>
2021-09-09 20:24:55 +0200

    Understanding tail-recursion

--- src/main/scala/retcalc/RetCalc.scala
+++ src/main/scala/retcalc/RetCalc.scala
@@ -1,5 +1,7 @@
 package retcalc
 
+import scala.annotation.tailrec
+
 object RetCalc {
   def futureCapital(interestRate: Double, nbOfMonths: Int, netIncome:
   Int, currentExpenses: Int, initialCapital: Double): Double = {
@@ -9,5 +11,42 @@ object RetCalc {
     )
 
 
+  }
+
+  def simulatePlan(interestRate: Double,
+                   nbOfMonthsSaving: Int, nbOfMonthsInRetirement: Int,
+                   netIncome: Int, currentExpenses: Int, initialCapital:
+                   Double): (Double, Double) = {
+    val capitalAtRetirement = futureCapital(
+      interestRate = interestRate, nbOfMonths = nbOfMonthsSaving,
+      netIncome = netIncome, currentExpenses = currentExpenses,
+      initialCapital = initialCapital)
+
+    val capitalAfterDeath = futureCapital(
+      interestRate = interestRate, nbOfMonths = nbOfMonthsInRetirement,
+      netIncome = 0, currentExpenses = currentExpenses,
+      initialCapital = capitalAtRetirement)
+
+    (capitalAtRetirement, capitalAfterDeath)
+  }
+
+  def nbOfMonthsSaving(interestRate: Double, nbOfMonthsInRetirement: Int,
+                       netIncome: Int, currentExpenses: Int, initialCapital: Double): Int = {
+    @tailrec
+    def loop(months: Int): Int = {
+      val (capitalAtRetirement, capitalAfterDeath) = simulatePlan(
+        interestRate = interestRate,
+        nbOfMonthsSaving = months, nbOfMonthsInRetirement =
+          nbOfMonthsInRetirement,
+        netIncome = netIncome, currentExpenses = currentExpenses,
+        initialCapital = initialCapital)
+
+      if (capitalAfterDeath > 0.0)
+        months
+      else
+        loop(months + 1)
+    }
+
+    loop(0)
   }
 }
--- src/test/scala/retcalc/RetCalcSpec.scala
+++ src/test/scala/retcalc/RetCalcSpec.scala
@@ -19,5 +19,51 @@ class RetCalcSpec extends AnyWordSpec with Matchers with TypeCheckedTripleEquals
       val expected = 541267.1990
       actual should ===(expected)
     }
+
+    "calculate how much savings will be left after having taken a pension fo n months" in {
+      val actual = RetCalc.futureCapital(
+        interestRate = 0.04 / 12,
+        nbOfMonths = 40 * 12,
+        netIncome = 0,
+        currentExpenses = 2000,
+        initialCapital = 541267.1990)
+      val expected = 309867.53176
+      actual should ===(expected)
+    }
+
   }
+  "RetCalc.simulatePlan" should {
+    "calculate the capital at retirement and the capital after death" in {
+      val (capitalAtRetirement, capitalAfterDeath) =
+        RetCalc.simulatePlan(
+          interestRate = 0.04 / 12,
+          nbOfMonthsSaving = 25 * 12, nbOfMonthsInRetirement = 40 * 12,
+          netIncome = 3000, currentExpenses = 2000,
+          initialCapital = 10000)
+      capitalAtRetirement should ===(541267.1990)
+      capitalAfterDeath should ===(309867.5316)
+    }
+  }
+
+  "RetCalc.nbOfMonthsSaving" should {
+    "calculate how long I need to save before I can retire" in {
+      val actual = RetCalc.nbOfMonthsSaving(
+        interestRate = 0.04 / 12, nbOfMonthsInRetirement = 40 * 12,
+        netIncome = 3000, currentExpenses = 2000, initialCapital = 10000)
+      val expected = 23 * 12 + 1
+      actual should ===(expected)
+    }
+
+    "not crash if the resulting nbOfMonths is very high" in {
+      val actual = RetCalc.nbOfMonthsSaving(
+        interestRate = 0.01 / 12, nbOfMonthsInRetirement = 40 * 12,
+        netIncome = 3000, currentExpenses = 2999, initialCapital = 0)
+      val expected = 8280
+      actual should ===(expected)
+    }
+
+    "not loop forever if I enter bad parameters" in pending
+  }
+
+
 }
